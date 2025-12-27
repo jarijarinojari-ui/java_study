4241160334f2229cae8927ec68138b804dad9888
kjg010829 <kjg010829@naver.com>
2025-12-27 17:54:53 +0900

    commit

---
+++ TestPjt/src/main/java/com/testPjt/HelloServlet.java
@@ -0,0 +1,57 @@
+package com.testPjt;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+//@WebServlet("/HS")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+response.setContentType("text/html; charset=UTF-8");
+		
+		// 2. 출력 도구 준비
+		PrintWriter out = response.getWriter();
+		
+		// 3. HTML 작성 (오타 수정됨)
+		out.print("<html>");
+		out.print("<head><title>Hello Servlet</title></head>");
+		out.print("<body>");
+		out.print("<h1>Hello Servlet!!</h1>");
+		out.print("<p>hello Sever</p>");
+		out.print("</body>");
+		out.print("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
