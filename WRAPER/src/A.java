f9585ec0ed42c9a5873577846fe178386b3d5636
kjg010829@naver.com <jarijarinojari@gmail.com>
2026-01-06 19:20:17 +0900

    commit

--- WRAPER/src/A.java
+++ WRAPER/src/A.java
@@ -1,8 +1,20 @@
 
 public class A {
 	void a() {
-		int a = 10;
-		Integer a2 = new Integer(a);
+		String s = "10";
+		Integer a2 = Integer.valueOf(s);
+		System.out.println(a2);
+		int a = a2;
+		
+		s = String.valueOf(a);
+		
+		
+	}
+	static void a2() {
+		Integer a = 100;
+		Integer b = 200;
+		int result = a.compareTo(b);
+		System.out.println(result);
 	}
 
 }
